c073c5504128b746fd43eb5220d1bc8d42691a01
akhil-nuox <akhil@nuox.io>
2024-06-27 11:19:44 +0530

    first push

---
+++ main.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+ 
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg);
+    socket.broadcast.emit('message', msg + "***" + new Date().toTimeString()); // Broadcast the message to all clients
+  });
+
+  socket.on("notification", (msg) => {
+    socket.broadcast.emit("notification", msg);
+  })
+  
+//   setInterval(() => {
+//     io.emit('message', 'Welcome!');
+//     console.log("message sent");
+//   }, 1000);
+});
+
+app.get("/notify/:message", (req, res) => {
+  const { message } = req.params;
+  io.emit("notification", message);
+  res.send("some notification sent");
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
